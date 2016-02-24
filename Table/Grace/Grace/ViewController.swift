0244dd034f93059d4b6f58105df3ff82d4f02abf
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-24 17:35:38 +0800

    Extension - roundCorners()

--- Table/Grace/Grace/ViewController.swift
+++ Table/Grace/Grace/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var no_1_View: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        no_1_View.roundCorners([.TopRight, .BottomRight], radius: no_1_View.frame.height/2)
     }
 
     override func prefersStatusBarHidden() -> Bool {
@@ -21,3 +24,11 @@ class ViewController: UIViewController {
 
 }
 
+extension UIView {
+    func roundCorners( corners: UIRectCorner, radius: CGFloat ) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+            mask.path = path.CGPath
+        self.layer.mask = mask
+    }
+}
