72d212f9b6926078f4ff68cc254f5c4d75ec6c75
EddieWen-Taiwan <m20021991y@gmail.com>
2016-03-07 16:30:50 +0800

    Add TableView

---
+++ To-Do/Goutham/Goutham/TaskTableCell.swift
@@ -0,0 +1,18 @@
+//
+//  TaskTableCell.swift
+//  Goutham
+//
+//  Created by Eddie on 3/7/16.
+//  Copyright © 2016 Wen. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- To-Do/Goutham/Goutham/ViewController.swift
+++ To-Do/Goutham/Goutham/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
     @IBOutlet var newTaskButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+
         newTaskButton.layer.cornerRadius = 30
     }
 
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("taskCell", forIndexPath: indexPath) as! TaskTableCell
+        return cell
     }
 
 }
