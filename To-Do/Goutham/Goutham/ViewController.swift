4154fcfda0226696dceaded3b6ff521b5c8b8fac
EddieWen-Taiwan <m20021991y@gmail.com>
2016-03-07 16:17:30 +0800

    Add task button at the bottom

--- To-Do/Goutham/Goutham/ViewController.swift
+++ To-Do/Goutham/Goutham/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var newTaskButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        newTaskButton.layer.cornerRadius = 30
     }
 
     override func prefersStatusBarHidden() -> Bool {
